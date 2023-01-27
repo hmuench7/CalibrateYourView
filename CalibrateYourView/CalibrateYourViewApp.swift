e8e203aaea6aa199e8e0422dee785af127e8c476
hmuench <hmuench@umich.edu>
2023-01-27 11:13:27 -0500

    Initial Commit

---
+++ CalibrateYourView/CalibrateYourViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalibrateYourViewApp.swift
+//  CalibrateYourView
+//
+//  Created by Hunter Muench on 1/27/23.
+//
+
+import SwiftUI
+
+@main
+struct CalibrateYourViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalibrateYourView/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CalibrateYourView
+//
+//  Created by Hunter Muench on 1/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
