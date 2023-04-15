0a42bbf68aa2e2221004a51d143963b5fa249551
hmuench7 <hunter.d.muench@gmail.com>
2023-04-15 17:32:51 -0400

    Created ApplyView

---
+++ CalibrateYourView/ApplyView.swift
@@ -0,0 +1,20 @@
+//
+//  ApplyView.swift
+//  CalibrateYourView
+//
+//  Created by Hunter Muench on 4/15/23.
+//
+
+import SwiftUI
+
+struct ApplyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ApplyView_Previews: PreviewProvider {
+    static var previews: some View {
+        ApplyView()
+    }
+}
