6aeac026eeefe3732bcb23ac7db2794f435a8ae6
hmuench7 <hunter.d.muench@gmail.com>
2023-04-17 00:59:15 -0400

    ApplyView has been updated to display based upon profile and system settings

--- CalibrateYourView/ApplyView.swift
+++ CalibrateYourView/ApplyView.swift
@@ -15,22 +15,22 @@ struct ApplyView: View {
     
     @State var boldText = UIAccessibility.isBoldTextEnabled;
     //@State var dyslexieFont = false;
-    @State var speakSelection = UIAccessibility.isSpeakSelectionEnabled;
-    @State var speakScreen = UIAccessibility.isSpeakScreenEnabled;
+//    @State var speakSelection = UIAccessibility.isSpeakSelectionEnabled;
+//    @State var speakScreen = UIAccessibility.isSpeakScreenEnabled;
     //@State var highlightContent = false; Display with speakSelection or speakScreen
     @Environment(\.colorScheme) private var colorScheme;
     //@State var trueTone = UIAccessibility.;
     //@State var displayZoom = UIAccessibility;
-    @State var onOffLabels = UIAccessibility.isOnOffSwitchLabelsEnabled;
-    @State var reduceTransparency = UIAccessibility.isReduceTransparencyEnabled;
-    @State var increaseContrast = UIAccessibility.isDarkerSystemColorsEnabled;
-    @Environment(\.accessibilityDifferentiateWithoutColor) private var differentiateWithoutColor
+//    @State var onOffLabels = UIAccessibility.isOnOffSwitchLabelsEnabled;
+//    @State var reduceTransparency = UIAccessibility.isReduceTransparencyEnabled;
+//    @State var increaseContrast = UIAccessibility.isDarkerSystemColorsEnabled;
+//    @Environment(\.accessibilityDifferentiateWithoutColor) private var differentiateWithoutColor
     //@State var reduceWhitePoint = UIAccessibility;
     //@State var onOffAutoBrightness = ;
-    @Environment(\.accessibilityReduceMotion) private var reduceMotion;
+//    @Environment(\.accessibilityReduceMotion) private var reduceMotion;
     //@State var autoPlayMessageEffects = ;
     //@State var dimFlashingLights = ;
-    @State var autoPlayVideoPreviews = UIAccessibility.isVideoAutoplayEnabled;
+//    @State var autoPlayVideoPreviews = UIAccessibility.isVideoAutoplayEnabled;
     //@State var limitFramerate = false;
     
     
@@ -39,7 +39,13 @@ struct ApplyView: View {
         ZStack {
             // Set Background
             Colors.SetBackground(isDarkmode: colorScheme == .dark)
+            
             SettingsToApply
+            
+            SingleNavButton(label: "Back to Profiles",
+                            destination: { ProfilesView() },
+                            action: {},
+                            isDarkmode: colorScheme == .dark)
         }
     }
     
@@ -47,17 +53,37 @@ struct ApplyView: View {
         ZStack {
             RoundedRectangle(cornerRadius: 10)
                 .fill(Colors.GetBackground2(isDarkmode: colorScheme == .dark))
-
+            let url = URL(string: UIApplication.openSettingsURLString)
             // Scrollable Settings Section
             ScrollView(.vertical, showsIndicators: false) {
                 Text("How To Apply Settings")
                     .font(.system(size: 20, weight: .bold))
                     .padding()
-                if currentProfile.isBold == boldText {
-                    let url = URL(string: UIApplication.openSettingsURLString)
+                if currentProfile.isBold != boldText {
                     Text("To Enable the Bold Text Setting")
+                    Link("Navigate to \"Settings > Display and Brightness\"",
+                    destination: url!)
+                    Text("Select the toggle next to \"Bold Text\"")
+                    Divider()
+                }
+                if currentProfile.darkmode != (colorScheme == .dark) {
+                    Text("To Enable the Dark Mode Setting")
                     Link("Navigate to Settings > Display and Brightness",
                     destination: url!)
+                    Text("Select \"Dark\" in the Appearance Header")
+                    Divider()
+                }
+                if currentProfile.largerText {
+                    Text("To Enable Larger Text Sizes")
+                    Link("Navigate to Settings > Accessibility > Display & Text Size > Larger Text",
+                    destination: url!)
+                    Text("Enable the toggle at the top of the page")
+                    Text("and then change the slider at the bottom")
+                    Text("to the desired size")
+                    Divider()
+                }
+                if !(currentProfile.isBold != boldText && currentProfile.darkmode != (colorScheme == .dark) && currentProfile.largerText) {
+                    Text("Yay! You have applied all the settings of this profile. Please click the button at the bottom to go back to the Profiles page.")
                 }
             }
             .padding()
